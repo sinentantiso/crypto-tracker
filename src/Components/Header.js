e3a39a4a99b5b0890c36feb16bb909a8b863627d
Sinethemba Ntantiso <103891865+sinentantiso@users.noreply.github.com>
2023-10-15 20:54:09 +0200

    Header update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,26 @@
+import { createTheme, makeStyles } from '@mui/material';
 import React from 'react';
 
-function MyComponent() {
+const useStyles = makeStyles ((theme) => ({
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+    },
+}))
+
+const darkTheme = createTheme ({
+    palette: {
+        primary: {
+          main: "#fff",
+        },
+        type: "dark",
+    },
+});
+
+function Header() {
     return (
         <div>
             Hello Sine
@@ -8,4 +28,4 @@ function MyComponent() {
     );
 }
 
-export default MyComponent;
+export default Header;
