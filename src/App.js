6612e9af696a2237a8296f21c8c47f8b05173f44
Sinethemba Ntantiso <103891865+sinentantiso@users.noreply.github.com>
2023-10-15 19:34:48 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello Sine
-    </div>
+    <BrowserRouter>
+      
+    </BrowserRouter>
   );
 }
 
