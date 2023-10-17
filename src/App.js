e1f46906890a05ab953514e401bc68ef9d01dab6
Sinethemba Ntantiso <103891865+sinentantiso@users.noreply.github.com>
2023-10-17 13:30:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./App.css";
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import Header from './Components/Header';
 
 
 function App () {
   return (
     <BrowserRouter>
       <div>
-        
+        <Header/>
       </div>
     </BrowserRouter>
   )
